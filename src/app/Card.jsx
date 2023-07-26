a9b2db8f69149f51021091a80a024b19d7c759af
habibhamdoun <100615843+habibhamdoun@users.noreply.github.com>
2023-07-26 04:29:15 +0300

    typography enhancements , added shadows,lowered scale

--- src/app/Card.jsx
+++ src/app/Card.jsx
@@ -17,12 +17,12 @@ const Card = ({
       onHoverStart={(e) => (isMobile ? ' ' : onHover(e))}
       id={id}
       className={
-        'flex flex-col justify-center items-center min-[1000px]:w-[33%]'
+        'flex flex-col justify-center items-center min-[1000px]:w-[33%] shadow-2xl'
       }
       initial={{ opacity: 0 }}
       whileInView={{ opacity: 1 }}
       animate={
-        !isMobile ? { scale: isHighlighted == id ? 1.1 : 0.85 } : { scale: 1 }
+        !isMobile ? { scale: isHighlighted == id ? 1.05 : 0.85 } : { scale: 1 }
       }
       transition={{
         opacity: { delay: animationDelay },
@@ -31,7 +31,7 @@ const Card = ({
     >
       <div
         className={
-          'bg-[#d6d5d5] w-[28vw] h-[8vh] rounded-t-[5px] max-[1000px]:text-sm text-base px-4 py-3 max-[1000px]:w-[50vw] max-[600px]:w-[60vw] flex items-center justify-center'
+          'bg-[#d6d5d5] w-[28vw] h-[8vh] rounded-t-[5px] max-[1000px]:text-sm font-semibold text-lg px-4 py-3 max-[1000px]:w-[50vw] max-[600px]:w-[60vw] flex items-center justify-center'
         }
       >
         {title}
@@ -40,7 +40,7 @@ const Card = ({
         className='w-[28vw] h-[50vh] aspect-square object-cover max-[1000px]:w-[50vw] max-[600px]:w-[60vw] max-[1000px]:h-[400px]'
         src={img}
       ></Image>
-      <p className='w-[28vw] max-[1000px]:h-fit rounded-b-lg h-[20vh] p-5 max-[1000px]:text-sm text-lg max-[1000px]:text-xl bg-[#d0d0ce] text-black max-[1000px]:w-[50vw] max-[600px]:w-[60vw] max-[600px]:text-base'>
+      <p className='w-[28vw] max-[1000px]:h-fit rounded-b-lg h-[15vh] p-5 max-[1000px]:text-sm text-lg max-[1000px]:text-xl bg-[#d0d0ce] text-black max-[1000px]:w-[50vw] max-[600px]:w-[60vw] max-[600px]:text-base'>
         {text}
       </p>
     </motion.div>
